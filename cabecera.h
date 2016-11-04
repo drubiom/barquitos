f95ff048222a9414399bc357ae29055258ea483a
drubiom <davidrubio.95@gmail.com>
2016-11-04 12:48:27 +0100

    UPDATE CABECERA.H
    cambiados los comentarios desde la fase de desarrollo a fase final para saber que hace cada funcion

--- cabecera.h
+++ cabecera.h
@@ -4,24 +4,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-//#include <windows.h>
+#include <windows.h>
 
 #include <unistd.h>
 
-char tablerouser [7][7];
-char tablerocompuvisible [7][7];
-char tablerocompuoculto [7][7];
-int turno, hundidoscompu, hundidosuser, num1, num2;
+char tablerouser [7][7];//INICIALIZA TABLERO USUARIO
+char tablerocompuvisible [7][7];//INICALIZA TABLERO CON LOS BARCOS DEL COMPU VISIBLE
+char tablerocompuoculto [7][7];//INICALIZA TABLERO CON LOS BARCOS DEL COMPU OCULTO
+int turno, hundidoscompu, hundidosuser, num1, num2;/*INICIALIZA LAS VARIABLES GLOBALES NECESARIAS (TURNO ACTUAL,
+NÚMERO DE BARCOS HUNDIDOS POR CADA USER)*/
 
-void colocacompu();//TERMINADA
-void colocauser();//TERMINADA
-void juegauser();//POR HACER
-void juegacompu();//POR HACER
-void imprimiruser();//TERMINADA
-void imprimircompu();//TERMINADA
-void inicializartableros();//TERMINADA
-int sorteaturno();//TERMINADA
-int comprobar(int num1, int num2, int tablero);//TERMINADA
-int comprobarBARCOS(int num1, int num2, int tablero);//POR HACER
+void colocacompu();//COLOCA LOS BARCOS DEL COMPU
+void colocauser();//COLOCA LOS BARCOS DEL USUARIO, SE PIDEN LAS POSICIONES AL USUARIO
+void juegauser();//JUEGA EL JUGADOR
+void juegacompu();//JUEGA COMPU
+void imprimiruser();//IMPRIME EL TABLERO DE USER
+void imprimircompu();//IMPRIME EL TABLERO CON LOS BARCOS OCULTOS DE COMPU
+void inicializartableros();//INICIALIZA LOS TABLEROS CON ASTERISCOS EN TODAS LAS POSICIONES
+int sorteaturno();//TNUMERO ALEATORIO PARA SORTEAR TURNO
+int comprobar(int num1, int num2, int tablero);
+int comprobarBARCOS(int num1, int num2, int tablero);
 int comprobarsihundido(int num1,int num2,int tablero);
+//COMPRUEBA SI LOS DISPARON DAN A UN BARCO, SI EL BARCO ESTÁ HUNDIDO
 #endif
